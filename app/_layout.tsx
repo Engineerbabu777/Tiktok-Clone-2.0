971c5b2eec3f3abb8c48bca0cbb2331331ab5907
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-09 12:25:52 +0500

    add(TabsLayout): this has been added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const queryClient = new QueryClient();
+
+  const myTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      primary: 'white',
+    }
+  }
+
+  return (
+    <ThemeProvider value={myTheme}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  )
 }
