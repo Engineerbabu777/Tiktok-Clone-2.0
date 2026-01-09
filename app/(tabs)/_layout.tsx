71df3dfb05a09a8fa2770e5a6180ef9581447108
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-09 12:12:40 +0500

    add(TabsLayout): this has been added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
+import { Entypo, Feather } from '@expo/vector-icons';
 import { Tabs } from "expo-router";
-
 
 
 
@@ -7,6 +7,12 @@ export default function TabsLayout() {
 
 
     return <Tabs>
-        <Tabs.Screen name="index" options={{ title: 'Home' }} />
+        <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} /> }} />
+        <Tabs.Screen name="friends" options={{ title: 'Home', tabBarIcon: ({ color }) => <Feather name="users" size={24} color={color} /> }} />
+        <Tabs.Screen name="newPost" options={{ title: 'Home', tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} /> }} />
+
+        <Tabs.Screen name="profile" options={{ title: 'Home', tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} /> }} />
+
+
     </Tabs>
 }
